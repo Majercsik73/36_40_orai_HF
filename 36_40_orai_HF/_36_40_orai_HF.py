bd6aa0999340fe6f95e73c2a620ad93a7a83f379
Majercsik73 <riasztotnekem@gmail.com>
2020-12-13 17:01:26 +0100

    a 2. feladat hozzáadása.

--- 36_40_orai_HF/_36_40_orai_HF.py
+++ 36_40_orai_HF/_36_40_orai_HF.py
@@ -17,5 +17,17 @@ def legrosszabb(legtobb):
         if legtobb < hy[i]:
             legtobb = hy[i]
     return legtobb
-print("A legmagasabb hiányzás az osztályban: " + str(legrosszabb(legtobb)))
+print("A legmagasabb hiányzási óraszám az osztályban: " + str(legrosszabb(legtobb)))
+print()
+
+# 2. feladat
+atlag = 0
+def atlaghy(atlag):
+    ossz = 0
+    for i in range(0,31):
+        ossz += hy[i]
+        atlag = round(ossz/31)
+    return atlag
+print("Az osztály hiányzásainak átlagos óraszáma: " + str(atlaghy(atlag)))
+
 print()
