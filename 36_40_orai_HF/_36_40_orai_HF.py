97e36a34edfbe686cdf9ae918601ea105fc926a1
Majercsik73 <riasztotnekem@gmail.com>
2020-12-13 20:56:10 +0100

    a 3. feladat hozzáadása.

--- 36_40_orai_HF/_36_40_orai_HF.py
+++ 36_40_orai_HF/_36_40_orai_HF.py
@@ -24,10 +24,63 @@ print()
 atlag = 0
 def atlaghy(atlag):
     ossz = 0
-    for i in range(0,31):
+    for i in range(0,len(hy)):
         ossz += hy[i]
         atlag = round(ossz/31)
     return atlag
 print("Az osztály hiányzásainak átlagos óraszáma: " + str(atlaghy(atlag)))
-
 print()
+
+# 3. feladat
+
+#atlagalatt = 0
+#for i in range (0,len(hy)):
+#    if(hy[i] < atlaghy(atlag)):
+#        atlagalatt += 1
+#print("A hónapban " + str(atlagalatt) + " diáknál volt átlag alatti a hiányzás!")
+#print()
+#print()
+
+hiany = 0
+def atlag(hiany):
+    elem = 0
+    for i in range(0,len(hy)):
+        if hy[i] == atlaghy(atlag):
+            elem += 1
+    return (elem)
+if atlag(hiany) > 0:
+    print(str(atlag(hiany)) + " diáknak volt az átlag hiányzással megegyező hiányzása!")
+else:
+    print("Nem volt olyan diák, akinek a hiányzása az átlaggal lenne egyenlő")
+print()
+
+diak = 0
+def atlagdiak(diak):
+    for i in range(0,len(hy)):
+        if hy[i] == atlaghy(atlag):
+            diak = i+1
+    return (diak)
+if atlagdiak(diak) == 0:
+    print(" ")
+else:
+    print("A " + str(atlagdiak(diak)) + " -dik számú diáknak volt az átlag hiányzással megegyező hiányzása!")
+print()
+
+
+#diak = 0
+#for i in range(0,len(hy)):
+#    if hy[i] == atlaghy(atlag):
+#        diak = i
+#print("A " + str(diak+1) + "-dik diáknak volt átlag körüli a hiányzása!")
+#print()
+
+#pont = 0
+#melyik = []
+#for i in range(0,31):
+#    if (hy[i] >= atlag-30) or (hy[i] <= atlag+30):
+#            pont = hy[i]
+#print(pont)
+#print(melyik)
+
+
+# 4. feladat 
